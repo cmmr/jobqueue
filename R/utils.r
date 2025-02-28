27e48afdf9f8d2f65e80182a4ab6eb4d5ca6cf58
Daniel Smith <dansmith@orst.edu>
2025-02-28 17:45:33 +0000

    CRAN release

--- R/utils.r
+++ R/utils.r
@@ -103,8 +103,11 @@ as_cnd <- function (reason, cls) {
 
 as_error <- function (reason) {
   cnd <- as_cnd(reason, 'error')
-  if (!inherits(cnd, 'error'))
-    class(cnd) %<>% { append(., 'error', which(. == 'condition') - 1) }
+  if (!inherits(cnd, 'error')) {
+    cls <- class(cnd)
+    cls <- append(cls, 'error', which(cls == 'condition') - 1)
+    class(cnd) <- cls
+  }
   return (cnd)
 }
 
