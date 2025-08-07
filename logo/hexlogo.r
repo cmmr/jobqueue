80a589798258fc4524642b244a89fe5d2c24c0a5
Daniel Smith <dansmith@orst.edu>
2025-08-07 19:34:44 +0100

    logo border

--- logo/hexlogo.r
+++ logo/hexlogo.r
@@ -7,7 +7,7 @@ ggplot() +
     mapping = aes(
       x = 1300 * sqrt(3)/2 * c(0, 1, 1, 0, -1, -1), 
       y = 1300 * .5 * c(2, 1, -1, -2, -1, 1) ),
-    color     = '#003087',
+    color     = '#0078a5',
     fill      = 'white',
     linewidth = 6 ) + 
   coord_fixed(ratio = 1) +
