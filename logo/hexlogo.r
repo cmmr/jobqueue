1e1a68bf5aac549268881f5265668d105a813fdc
Daniel Smith <dansmith@orst.edu>
2025-08-07 19:15:23 +0100

    tweaked the logo

--- logo/hexlogo.r
+++ logo/hexlogo.r
@@ -1,53 +1,46 @@
 
 library(ggplot2)
+library(magick)
 
 ggplot() + 
-  ggpattern::geom_polygon_pattern(
+  geom_polygon(
     mapping = aes(
-      x = 1200 * sqrt(3)/2 * c(0, 1, 1, 0, -1, -1), 
-      y = 1200 * .5 * c(2, 1, -1, -2, -1, 1) ),
-    pattern          = 'image',
-    pattern_type     = 'fit',
-    pattern_filename = 'logo/cartoon.png',
-    color            = 'black',
-    fill             = 'white',
-    linewidth        = 4 ) + 
+      x = 1300 * sqrt(3)/2 * c(0, 1, 1, 0, -1, -1), 
+      y = 1300 * .5 * c(2, 1, -1, -2, -1, 1) ),
+    color     = '#003087',
+    fill      = 'white',
+    linewidth = 6 ) + 
   coord_fixed(ratio = 1) +
   theme_void() +
   theme(rect = element_rect(fill = 'transparent')) +
   annotate(
-    geom   = 'text', 
-    label  = 'jobqueue',
-    family = 'Brownland',
-    size   = 20,
-    x      = 0, 
-    y      = -580 )
+    geom     = 'text', 
+    label    = 'jobqueue',
+    family   = 'Titillium Web',
+    fontface = 'bold',
+    color    = '#181a1b',
+    size     = 20,
+    x        = 0, 
+    y        = -550 ) +
+  annotation_raster(
+    raster = image_read('logo/cartoon.png'),
+    xmin = -868, xmax = 868,
+    ymin = -393, ymax = 993 )
 
 
 
 ggsave(
   path     = 'logo',
-  filename = 'interprocess.png', 
+  filename = 'jobqueue.png', 
   device   = 'png',
-  width    = 2000, 
-  height   = 2000, 
-  dpi      = 380, 
+  width    = 2600, 
+  height   = 2600, 
+  dpi      = 400, 
   units    = 'px',
   bg       = 'transparent' )
 
 
-library(magick)
-library(magrittr)
-
-# pkgdown website sets logo width to 120px
-image_read('logo/jobqueue.png') %>%
-  image_trim() %>%
-  image_resize('120x') %>%
+image_read('logo/jobqueue.png') |>
+  image_trim() |>
+  image_resize('x200') |>
   image_write('man/figures/logo.png')
-
-
-# height = 200px (150pt) for joss paper
-image_read('logo/jobqueue.png') %>%
-  image_trim() %>%
-  image_resize('x200') %>%
-  image_write('joss/figures/logo.png')
